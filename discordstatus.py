0c7b43f8a5725f6339e899dd1e83286433ab45ff
adminus <adminus@adminus.adminus>
2025-03-30 04:04:09 +0300

    dddd

--- discordstatus.py
+++ discordstatus.py
@@ -177,4 +177,4 @@ class DiscordStatusCombinedMod(loader.Module):
         if self.bot and self.bot_ready:
             await self.bot.close()
         if self.client and self.client_ready:
-            await self.client.close()
+            await self._client.close()
